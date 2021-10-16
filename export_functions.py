c87d437e7e673593def8c850993124d0a3f99560
NeLiS-JMW <nielsvoogd@xs4all.nl>
2021-10-16 19:03:24 +0200

    rebuilt for new geojson format

--- export_functions.py
+++ export_functions.py
@@ -1,33 +1,74 @@
-import json
+import geojson
+from geojson import Feature, MultiPolygon
 
 
 def dat_export(file_name, output_dir):
     print('file_name {}'.format(file_name))
     print('output_dir {}'.format(output_dir))
-    with open(file_name) as json_file:
-        data = json.load(json_file)
+
+    with open(file_name) as f:
+        gj = geojson.load(f)
+        split_id = []
+        u_id = {}
+        new_feature_list = []
+        print(gj['name'])
+
+        for feature in gj['features']:
+            if feature['properties']['id'] not in u_id.keys():
+                u_id[feature['properties']['id']] = feature['properties']['oceanic']
+                feature['properties']['extension'] = '0'
+            else:
+                if u_id[feature['properties']['id']] == feature['properties']['oceanic']:
+                    feature['properties']['extension'] = '1'
+                else:
+                    feature['properties']['extension'] = '0'
+            # need to be split
+            if len(feature['geometry']['coordinates']) > 1:
+                extension = False
+                for polygon in feature['geometry']['coordinates']:
+                    if feature['properties']['id'] not in split_id:
+                        extension = '0'
+                        split_id.append(feature['properties']['id'])
+                    else:
+                        extension = '1'
+                    polygon = MultiPolygon([[[tuple(x) for x in polygon[0]]]])
+                    new_feature = Feature(geometry=polygon, properties={'id': feature['properties']['id'],
+                                                                            'oceanic': feature['properties'][
+                                                                                'oceanic'],
+                                                                            'extension': extension,
+                                                                            'label_lon': feature['properties'][
+                                                                                'label_lon'],
+                                                                            'label_lat': feature['properties'][
+                                                                                'label_lat']
+                                                                            })
+                    new_feature_list.append(new_feature)
+
+            else:
+                # no need to be split
+                new_feature_list.append(feature)
+
+        gj['features'] = new_feature_list
+
+        # start building output
         f = open(output_dir, 'w')
-        for item in data['features']:
-            coordinates = item['geometry']['coordinates'][0][0]
-            feature = item['properties']
-            if 'IsExtensio' in feature:
-                header_line = '{}|{}|{}|{}|{}|{}|{}|{}|{}|{}\n'.format(feature['ICAO'], feature['IsOceanic'],
-                                                                       feature['IsExtensio'], len(coordinates),
-                                                                       round(float(feature['MinLat']), 6),
-                                                                       round(float(feature['MinLon']), 6),
-                                                                       round(float(feature['MaxLat']), 6),
-                                                                       round(float(feature['MaxLon']), 6),
-                                                                       round(float(feature['CenterLat']), 6),
-                                                                       round(float(feature['CenterLon']), 6))
-            elif 'IsExtension' in feature:
-                header_line = '{}|{}|{}|{}|{}|{}|{}|{}|{}|{}\n'.format(feature['ICAO'], feature['IsOceanic'],
-                                                                       feature['IsExtension'], len(coordinates),
-                                                                       round(float(feature['MinLat']), 6),
-                                                                       round(float(feature['MinLon']), 6),
-                                                                       round(float(feature['MaxLat']), 6),
-                                                                       round(float(feature['MaxLon']), 6),
-                                                                       round(float(feature['CenterLat']), 6),
-                                                                       round(float(feature['CenterLon']), 6))
+        for feature in gj['features']:
+            coordinates = feature['geometry']['coordinates'][0][0]
+            properties = feature['properties']
+
+            point_count = len(coordinates)
+            min_lat = min(coordinates, key=lambda x: x[1])[1]
+            min_lon = min(coordinates, key=lambda x: x[0])[0]
+            max_lat = max(coordinates, key=lambda x: x[1])[1]
+            max_lon = max(coordinates, key=lambda x: x[0])[0]
+
+            header_line = '{}|{}|{}|{}|{}|{}|{}|{}|{}|{}\n'.format(properties['id'], properties['oceanic'],
+                                                                   properties['extension'], point_count,
+                                                                   min_lat,
+                                                                   min_lon,
+                                                                   max_lat,
+                                                                   max_lon,
+                                                                   properties['label_lat'],
+                                                                   properties['label_lon'])
             f.write(header_line)
             for coord in coordinates:
                 val1 = round(coord[1], 6)
@@ -38,5 +79,5 @@ def dat_export(file_name, output_dir):
 
 
 if __name__ == "__main__":
-    dat_export('F:/Documents/Dutchvacc/SCT werk/VATSPY/PR.328/328.geojson',
-               'F:/Documents/Dutchvacc/SCT werk/VATSPY/PR.328/328test.dat')
+    dat_export(r'C:\Users\niels\PycharmProjects\vatspy-geojson\newdatafile_test\DAT\test\converted to gj\firboundaries.geojson',
+               r'C:\Users\niels\PycharmProjects\vatspy-geojson\newdatafile_test\DAT\Firboundaries.dat')
